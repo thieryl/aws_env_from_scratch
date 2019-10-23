a6c512eca67a6d074cf3a7d7b53d2dc683b7f4a2
Thiery Louison <thiery.louison@gmail.com>
2019-10-23 20:46:25 +0400

    Add vpc creation

---
+++ cout_vpc.tf
@@ -0,0 +1,10 @@
+# Create aws VPC
+resource "aws_vpc" "terraformmain" {
+  cidr_block = "${var.vpc_cidr}"
+  #### this 2 true values are for use the internal vpc dns resolution
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags {
+    Name = "My terraform vpc"
+  }
+}
